3a8c9c00922b8ea82a941b6db5f788b07d4605b4
Jiri Kucera <jkucera@redhat.com>
2021-01-27 22:30:00 +0100

    Add tests

--- filter_plugins/generate_inventory.py
+++ filter_plugins/generate_inventory.py
@@ -1,28 +1,27 @@
 # SPDX-License-Identifier: MIT
 
+
 class FilterModule(object):
     """Custom Ansible filter for generating static inventory"""
 
     def filters(self):
-        return {
-            "generate_inventory": self.generate_inventory
-        }
+        return {"generate_inventory": self.generate_inventory}
 
-    def generate_inventory(self, hosts, variables, role_name, extra_vars=None):
+    def generate_inventory(self, hostvars, variables, role_name, extra_vars=None):
         """
         Generate a content of the static inventory.
 
-        :param list hosts: A list of hosts.
+        :param dict hostvars: A dict of hosts variables.
         :param dict variables: A dict of all variables.
         :param str role_name: A name of an Ansible role.
         :param list extra_vars: A list of names of extra variables from
             `variables`.
         :returns: A content of a static inventory (:class:`dict`).
 
-        For each host in hosts, add all variables starting with `{role_name}_`
-        or `__{role_name}_` prefix to host. Additionally, add also all
-        variables from extra_vars to every host. All other variables are
-        ignored.
+        For each host in hostvars, add all variables starting with
+        `{role_name}_` or `__{role_name}_` prefix to host. Additionally,
+        add also all variables from extra_vars to every host. All other
+        variables are ignored.
         """
         extra_vars = extra_vars or []
         # Strip prefix from the role_name:
@@ -35,10 +34,7 @@ class FilterModule(object):
         # Generate a dictionary of host variables:
         host_variables = {}
         for v in variables:
-            if v.startswith(rv_prefix) or v.startswith(prv_prefix) \
-            or v in extra_vars:
+            if v.startswith(rv_prefix) or v.startswith(prv_prefix) or v in extra_vars:
                 host_variables[v] = variables[v]
 
-        return { 
-            "all": { "hosts": dict([(h, host_variables) for h in hosts]) }
-        }
+        return {"all": {"hosts": dict([(h, host_variables) for h in hostvars])}}
